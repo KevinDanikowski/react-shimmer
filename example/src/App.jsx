80b02c598e7363f751cb67675687b4e9be4a7f0d
Kevin Danikowski <kdanikowski@outlook.com>
2021-01-19 17:00:29 -0600

    added material ui core

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -5,7 +5,7 @@ import { TestComponent } from '@kevindanikowski/react-test-components'
 export default function App() {
   return (
     <div>
-      {/* <ExampleComponent /> */}
+      <TestComponent />
       <Image
         src='https://source.unsplash.com/random/800x600'
         fallback={<Shimmer width={800} height={600} />}
--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
 
 export const TestComponent = () => {
-  return <div>TEST</div>
+  return (
+    <>
+      <Button>Test Button</Button>
+      <div>TEST</div>
+    </>
+  )
 }
